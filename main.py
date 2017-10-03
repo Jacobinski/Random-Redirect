14e899d7ef2e40205193877e5864f3cb19c521d5
Jacob Budzis <jrbudzis@alumni.ubc.ca>
2017-10-02 22:46:48 -0600

    Add flask redirection code
    Flask code that redirects a user based on the websites in the
    websites list. This can be used on Heroku.

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from flask import Flask, redirect
+app = Flask(__name__)
+
+# Note: http:// or https:// must be the prefix for proper redirection
+websites = [
+        "http://www.github.com",
+        "https://www.twitter.com",
+        "http://www.reddit.com",
+    ]
+
+@app.route('/')
+def index():
+    website = random.choice(websites)
+    return redirect(website)
+
